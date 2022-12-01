a6889000578fa2c7b0d761db314413604e4d57a4
Khang <nkhang.dhkl16a1hn@sv.uneti.edu.vn>
2022-12-01 23:45:05 +0700

    them chuong 5

---
+++ C05/baitap5.1.2.py
@@ -0,0 +1,8 @@
+#viết chương trình toán học ,+,-,*,/
+#Sửa code baitap5.1.2.py,trong đó giá trị x,y được nhập từ bàn phím
+x=10
+y=5
+print("tổng x+y=",x+y)
+print ("hiệu x-y=",x-y)
+print ("tích x*y=",x*y)
+print ("thương x/y=",x/y)
---
+++ C05/baitap5.1.2a.py
@@ -0,0 +1,14 @@
+#viết chương trình toán học ,+,-,*,/
+#Sửa code baitap5.1.2.py,trong đó giá trị x,y được nhập từ bàn phím
+print("nhập các giá trị x,y từ bàn phím")
+x=int(eval(input("x= ")))
+y=int(eval(input("y= ")))
+tong = x+y
+hieu = x-y
+tich = x*y
+thuong = float(x/y)
+
+print("tổng=", tong)
+print("hiệu= ",hieu)
+print ("tich= ",tich)
+print ("thương= ",thuong)
